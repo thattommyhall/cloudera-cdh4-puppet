0ce8a02e39169bb1d8c0b2a810a0ec908cbe008e
thattommyhall <thattommyhall@gmail.com>
2013-01-15 16:29:34 +0000

    added rule of thumb for mapppers and reducers based on CPU

--- modules/hadoop/lib/facter/facts.rb
+++ modules/hadoop/lib/facter/facts.rb
@@ -5,3 +5,15 @@ end
 Facter.add("hadoop_mapred_local_dirs") do
   setcode { Dir.glob('/data*/mapred/local/').join(',') }
 end
+
+mapper_ratio = 0.8
+oversubscription = 0.2
+total_slots = (Facter.processorcount.to_i * (1+oversubscription)).ceil
+
+Facter.add("mapred_tasktracker_reduce_tasks_maximum") do
+  setcode { (total_slots * (1-mapper_ratio)).ceil }
+end
+
+Facter.add("mapred_tasktracker_map_tasks_maximum") do
+  setcode { (total_slots * mapper_ratio).ceil }
+end
